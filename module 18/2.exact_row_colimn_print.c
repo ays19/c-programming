68658e742f8dd77381abacd7ca2cda755b3e747e
ays19 <sahsanyasir@gmail.com>
2026-01-27 21:16:37 +0600

    row column print

---
+++ module 18/2.exact_row_colimn_print.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main()
+{
+    int row,col;
+    scanf("%d %d", &row, &col);
+    int arr[row][col];
+    for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++){
+            //printf("a[%d][%d] ",i,j);
+            scanf("%d", &arr[i][j]);
+        }
+    }
+
+    //output exact array
+    int e;
+    scanf("%d", &e);
+     for(int i=0;i<col;i++)
+
+        {
+            
+            printf("%d ", arr[e][i]);
+        }
+        printf("\n");
+    }
+    
